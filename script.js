05cdaf800e29c1199c56fef0edb3e7ea8e45a5ff
nmrrahma3-collab <nmrrahma3@gmail.com>
2025-12-19 23:16:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+function openMenu() {
+  document.getElementById("menu").style.right = "0";
+}
 
+function closeMenu() {
+  document.getElementById("menu").style.right = "-300px";
+}
+
+const elements = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+elements.forEach(el => observer.observe(el));
