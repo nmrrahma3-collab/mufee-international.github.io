70c64cd35ded3b79307cd651c453c568ca6bfe9b
nmrrahma3-collab <nmrrahma3@gmail.com>
2025-12-30 20:06:16 +0530

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,1 +1,20 @@
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  },
+  {
+    threshold: 0.15
+  }
+);
 
+document.querySelectorAll(
+  ".nation-card, .brand-story, .brand-cta"
+).forEach(el => {
+  observer.observe(el);
+});
